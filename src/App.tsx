e12b7ca2e1ccafdf4db14d65b0bfa81051a8d633
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2024-02-28 20:37:03 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/MyComponents/Navbar";
+
+function App() {
+  return (
+    <div className="w-[100dvw] h-[100dvh] bg-zinc-100 overflow-x-hidden overflow-y-auto">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
